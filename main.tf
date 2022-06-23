b57b551bb24d841881103df10f6a0f7d3271a9b1
Sree_mugesh <50001213@mahindra.com>
2022-06-23 17:44:08 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+locals {
+  name = "VM"
+}
+resource "aws_instance" "web-server" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = var.associate_public_ip_address
+  key_name                    = aws_key_pair.generated_key.key_name
+  monitoring                  = var.monitoring
+
+  root_block_device {
+    volume_size           = var.volume_size
+    delete_on_termination = var.delete_on_termination
+  }
+
+  tags = {
+    Name = "${local.name}"
+  }
+  
+}
+
+resource "tls_private_key" "example" {
+  algorithm = var.algorithm
+  rsa_bits  = var.rsa_bits
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
